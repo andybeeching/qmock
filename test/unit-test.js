e3f332456e14c213274c7221a8f9530fdc065e74
Andy Beeching <andybeeching@gmail.com>
2012-10-21 15:45:33 +0200

    ported stubbed property TC

--- test/unit-test.js
+++ test/unit-test.js
@@ -6,6 +6,10 @@
   var buster = global.buster || require( "buster" );
   var QMock = global.QMock || require( "../src/qmock" );
 
+  // Assertion aliases for brevity
+  var equals = assert.equals;
+  var fail = buster.assertions.fail;
+
   // Assign comparison function
   QMock.config.compare = buster.assertions.deepEqual;
 
@@ -56,7 +60,7 @@
       // Reset from namespace
       mock.foo.reset();
       // Check the property is back to setup value
-      assert.equals( mock.foo.faz, true );
+      equals( mock.foo.faz, true );
 
       // Change the property again
       mock.foo.faz = false;
@@ -64,9 +68,161 @@
       // Reset from parent mock
       mock.reset();
       // Check the property is back to setup value
-      assert.equals( mock.foo.faz, true );
+      equals( mock.foo.faz, true );
+    },
+
+    "Mock#excise [utility] method": function () {
+      var mock = new Mock(null, false); // plain namespace/receiver
+
+      // EXERCISE
+      mock.excise();
+
+      // VERIFY: Plain object (i.e. a namespace, not a fn obj)
+      var result = true;
+      for (var i in mock) {
+        result = false; break;
+      }
+      assert(result, "excised mock shouldn't have any properties on itself");
+
+      // SETUP - Custom Properties
+      // Object with a method 'foo', property 'bar', and namespace 'buz'
+      mock = new Mock({
+        // method foo()
+        "foo": {
+          "id"        : "foobarbaz",
+          "accepts"   : "foo",
+          "receives"  : {"accepts": "foo", fixture: "stub", returns: "bar"},
+          "returns"   : "bar",
+          "required"  : 1,
+          // nested namespace "faz"
+          "faz"       : {},
+          "overload"  : true,
+          "fixture"   : "response",
+          "async"     : true,
+          "chain"     : {},
+          "calls"     : [1,2],
+          // nested property "key"
+          "key"       : true
+        },
+        // property called 'bar'
+        "bar": "stub",
+        // namespace called 'buz' with method
+        "buz": {
+          // Test method setup
+          "baz": {
+            "accepts": "test"
+          },
+          // Test property setup
+          "daz": "stub",
+          // Test nested namspace setup
+          "gaz": {}
+        }
+      }, false);
+
+      // excise all mocks
+      mock.excise();
+
+      function verifyMock ( obj, keys ) {
+        var result = true, key;
+        for (var key in mock ) {
+          if ( obj.hasOwnProperty( key ) && !keys.exec( key )[0] ) {
+            result = false;
+            break;
+          }
+        }
+        return result;
+      }
+
+      // verify
+      assert(typeof mock == "object", "The mock should be a plain receiver and not a function.");
+      assert(verifyMock( mock, /foo|bar|buz|/ ), "The parent mock receiver should only have the properties ('foo', 'bar', 'buz') set upon it.");
+      assert(verifyMock( mock.foo, /faz|key/ ), "The mock function 'mock.foo()' should only have the properties ('faz', 'key') set upon it.");
+      assert(verifyMock( mock.buz, /baz|daz|gaz/ ), "The mock receiver 'mock.buz' should only have the properties ('baz', 'daz', 'gaz') set upon it.");
 
     }
+
+  });
+
+  /*
+   * The following tests use Mock#verify to produce a result to assert,
+   * rather than inspecting the internal state of each mock instance (which)
+   * would make the tests brittle and violate the Law of Demeter.???
+   * Although this approach differs from the usual TDD style of atomic method
+   * assertions, (invoke a method and assert on some returned object, or
+   * associated state), it has been deemed more pragmatic than testing every
+   * combination of expectations that might be desired.
+   */
+
+  buster.testCase("QMock: Mock behaviour (properties & methods)", {
+
+    "multiple stubbed properties": function () {
+
+      var ninja = new Mock;
+      // EXERCISE - invalid property naming (shadow QMock API)
+      try {
+        ninja.property('expects');
+        fail("Expected 'InvalidPropertyNameException'");
+      } catch (e) {
+        equals(e.type, "InvalidPropertyNameException");
+      }
+
+      var ninja = (new Mock)
+                    .property("rank", "apprentice");
+
+      equals( ninja.rank, "apprentice" )
+
+      ninja = (new Mock) // with 'new' keyword
+                .property("rank", "apprentice")
+                .property("master", "The Chrome");
+
+      equals(ninja.rank, "apprentice");
+      equals(ninja.master, "The Chrome");
+
+    // Composite
+    var samurai = Mock() // without new
+                    .property("rank", "apprentice")
+                    .method("swing")
+                  .end()
+                    .property("master", "The Chrome");
+
+    // EXERCISE
+    samurai.swing();
+
+    // VERIFY
+    assert( samurai.verify() );
+    equals(ninja.rank, "apprentice");
+    equals(ninja.master, "The Chrome");
+
+ 	  // Test all object types can be stored on property
+
+ 	  function Custom () {};
+
+ 	  var wizard = (new Mock)
+      .property("number", 1)
+      .property("boolean", true)
+      .property("string", "foo")
+      .property("null", null)
+      .property("undefined", undefined)
+      .property("function", function stubbedFunction () {})
+      .property("object", {})
+      .property("array", [])
+      .property("regExp", /RegExp/)
+      .property("date", new Date(1970))
+      .property("custom object", new Custom);
+
+ 	  // No need to exercise - all stubs
+ 	  equals( wizard["number"], 1, "wizard mock object should have a stubbed property of 'number' with a value of (Number: 1)");
+ 	  equals( wizard["boolean"], true, "wizard mock object should have a stubbed property of 'number' with a value of (Boolean: true)");
+ 	  equals( wizard["null"], null, "wizard mock object should have a stubbed property of 'null' with a value of (null)");
+ 	  equals( typeof wizard["function"], "function", "wizard mock object should have a stubbed property of 'function' with a value of (Function: function stubbedFunction () {})");
+ 	  equals( typeof wizard["object"], "object", "wizard mock object should have a stubbed property of 'object' with a value of (Object: {})");
+ 	  equals( wizard["array"].constructor, Array, "wizard mock object should have a stubbed property of 'array' with a value of (Array: [])");
+ 	  equals( wizard["regExp"].constructor, RegExp, "wizard mock object should have a stubbed property of 'regExp' with a value of (RegExp: /RegExp/)");
+ 	  equals( wizard["date"].constructor, Date, "wizard mock object should have a stubbed property of 'date' with a value of (Date: new Date)");
+ 	  equals( wizard["custom object"].constructor, Custom, "wizard mock object should have a stubbed property of 'custom object' with a value of (Custom: new Custom)");
+
+    }
+
   });
 
 }(this));
